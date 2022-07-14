60f11cf573766016076740543c671c3db51f8772
Huanran Chen <1062446749@qq.com>
2022-07-14 17:35:12 +0800

    Update track2_run.sh

--- track2_run.sh
+++ track2_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python total_train.py --parallel --batchsize 64 --fp16 --track_mode track2 \
+python total_train.py --parallel --batchsize 192 --fp16 --track_mode track2 \
         --train_image_path /home/Bigdata/NICO2/nico/train/ --label2id_path \
         /home/Bigdata/NICO2/ood_label_id_mapping.json --test_image_path /home/Bigdata/NICO2/nico/test/
