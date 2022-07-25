a7fba12eda3c140399f251564f571f36e915c778
shaoshitong <1090784053@qq.com>
2022-07-25 23:55:09 +0800

    111

--- ensemble_track1_run.sh
+++ ensemble_track1_run.sh
@@ -4,6 +4,12 @@ mkdir prediction
 
 chmod 777 track1_run.sh
 
+python train.py --test --batch_size 48 --img_size 384 --cutmix_in_cpu --track_mode track1 \
+        --train_image_path /home/Bigdata/NICO/nico/train/ --label2id_path \
+        /home/Bigdata/NICO/dg_label_id_mapping.json --test_image_path /home/Bigdata/NICO/nico/test/
+
+mv prediction.json prediction/prediction_0.json
+
 bash track1_run.sh
 
 python train.py --test --batch_size 48 --img_size 384 --cutmix_in_cpu --track_mode track1 \
@@ -19,6 +25,8 @@ python train.py --test --batch_size 48 --img_size 384 --cutmix_in_cpu --track_mo
         /home/Bigdata/NICO/dg_label_id_mapping.json --test_image_path /home/Bigdata/NICO/nico/test/
 
 mv prediction.json prediction/prediction_2.json
+
+python ensemble.py
 
 bash track1_run.sh
 
